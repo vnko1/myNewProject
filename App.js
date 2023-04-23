ab429464c3631d34bfdc80f2eb0153087addb33f
Andrii Valenko <andreyvalenko@gmail.com>
2023-04-23 23:59:46 +0300

    eds

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import { StatusBar } from "expo-status-bar";
+import { useState, useCallback } from "react";
 import { StyleSheet, View, ImageBackground } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
 import RegistrationScreen from "./screens/RegistrationScreen";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <ImageBackground
         style={styles.image}
         source={require("./assets/images/bg.jpg")}
--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -1,32 +1,107 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
+  Pressable,
   View,
   Text,
   TextInput,
-  ImageBackground,
+  Image,
   StyleSheet,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
 } from "react-native";
 
+const initialValue = { login: "", email: "", password: "" };
+
 export default RegistrationScreen = () => {
+  const [inputValue, setInputValue] = useState(initialValue);
   const [hiddenPassword, setHiddenPassword] = useState(true);
+
+  useEffect(() => {}, []);
+
+  const onPressBtn = () => console.log(inputValue);
   return (
     <View style={styles.container}>
-      <Text>Регистрация</Text>
-      <View>
-        <TextInput caretHidden={true} placeholder={"Логин"}></TextInput>
+      <Image
+        style={{
+          ...styles.image,
+          transform: [{ translateX: -60 }],
+        }}
+      />
+      <Text style={styles.title}>Регистрация</Text>
+      <View style={styles.form}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={{
+              ...styles.input,
+              backgroundColor: inputValue.login ? "#fff" : "#F6F6F6",
+              borderColor: inputValue.login ? "#FF6C00" : "#E8E8E8",
+            }}
+            placeholder={"Логин"}
+            onChangeText={(value) =>
+              setInputValue((prevState) => ({ ...prevState, login: value }))
+            }
+            value={inputValue.login}
+          ></TextInput>
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={{
+              ...styles.input,
+              backgroundColor: inputValue.email ? "#fff" : "#F6F6F6",
+              borderColor: inputValue.email ? "#FF6C00" : "#E8E8E8",
+            }}
+            placeholder={"Адрес электронной почты"}
+            onChangeText={(value) =>
+              setInputValue((prevState) => ({ ...prevState, email: value }))
+            }
+            value={inputValue.email}
+          ></TextInput>
+        </View>
+        <View style={{ ...styles.inputContainer, position: "relative" }}>
+          <TextInput
+            style={{
+              ...styles.input,
+              backgroundColor: inputValue.password ? "#fff" : "#F6F6F6",
+              borderColor: inputValue.password ? "#FF6C00" : "#E8E8E8",
+            }}
+            secureTextEntry={hiddenPassword}
+            placeholder={"Пароль"}
+            onChangeText={(value) =>
+              setInputValue((prevState) => ({ ...prevState, password: value }))
+            }
+            value={inputValue.password}
+          ></TextInput>
+          <Pressable
+            style={styles.passwordBtn}
+            onPress={() => setHiddenPassword((prevState) => !prevState)}
+          >
+            <Text style={styles.passwordBtnText}>
+              {hiddenPassword ? "Показать" : "Скрыть"}
+            </Text>
+          </Pressable>
+        </View>
       </View>
-      <View>
-        <TextInput
-          caretHidden={true}
-          placeholder={"Адрес электронной почты"}
-        ></TextInput>
+      <View style={{ ...styles.btnContainer, marginBottom: 16 }}>
+        <TouchableOpacity
+          onPress={onPressBtn}
+          activeOpacity={0.8}
+          disabled={
+            inputValue.login !== "" &&
+            inputValue.email !== "" &&
+            inputValue.password !== ""
+              ? false
+              : true
+          }
+        >
+          <View style={styles.regBtn}>
+            <Text style={styles.btnText}>Зарегистрироваться</Text>
+          </View>
+        </TouchableOpacity>
       </View>
-      <View>
-        <TextInput
-          secureTextEntry={hiddenPassword}
-          caretHidden={true}
-          placeholder={"Пароль"}
-        ></TextInput>
+      <View style={{ ...styles.btnContainer, alignItems: "center" }}>
+        <Pressable style={styles.link}>
+          <Text style={styles.linkText}>Уже есть аккаунт? Войти</Text>
+        </Pressable>
       </View>
     </View>
   );
@@ -34,9 +109,84 @@ export default RegistrationScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
+    position: "relative",
     backgroundColor: "#fff",
     width: "100%",
-    height: "50%",
+    height: "70%",
     borderRadius: "25px 25px 0px 0px",
+
+    alignItems: "center",
+  },
+  image: {
+    position: "absolute",
+    left: "50%",
+    top: -60,
+    borderRadius: 16,
+    width: 120,
+    height: 120,
+    backgroundColor: "#F6F6F6",
+  },
+  title: {
+    marginTop: 92,
+    marginBottom: 32,
+    fontFamily: "Roboto-Medium",
+    fontWeight: 500,
+    fontSize: 30,
+    lineHeight: 35,
+    letterSpacing: 0.01,
+    color: "#212121",
+  },
+  form: {
+    width: "100%",
+    gap: 16,
+    marginBottom: 43,
+  },
+  inputContainer: {
+    marginHorizontal: 32,
+  },
+  input: {
+    borderColor: "#E8E8E8",
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 16,
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#212121",
+  },
+
+  passwordBtn: {
+    position: "absolute",
+    top: 16,
+    right: 16,
+  },
+  passwordBtnText: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#1B4371",
+  },
+  btnContainer: { width: "100%" },
+  regBtn: {
+    backgroundColor: "#FF6C00",
+    marginHorizontal: 32,
+    paddingTop: 16,
+    paddingBottom: 16,
+    borderRadius: 100,
+  },
+  btnText: {
+    textAlign: "center",
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#fff",
+  },
+
+  // link: { textAlign: "center" },
+  linkText: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#1B4371",
   },
 });
